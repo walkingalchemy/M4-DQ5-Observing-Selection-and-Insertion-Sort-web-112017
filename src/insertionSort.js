612f787580a8c83b6aa448d3dd2658b7cf44e674
walkingalchemy <walkingalchemy@gmail.com>
2018-02-02 14:25:21 -0500

    Build selection and insertions sort

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,12 @@
 function insertionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+  // let start = 0
+  for (let start = 0; start < arr.length; start++){
+    for (let current = start; current > 0 && arr[current-1] > arr[current]; current-- ) {
+      let swap = arr[current]
+      arr[current] = arr[current-1]
+      arr[current-1] = swap
+    }
+  }
 }
--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,16 @@
 function selectionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
+
+  for (let current = 0; current < arr.length; current++) {
+    let mindex = current
+    for (let test = current; test < arr.length; test++) {
+      if (arr[test] < arr[mindex]){
+        mindex = test
+      }
+    }
+    let swap = arr[mindex]
+    arr[mindex] = arr[current]
+    arr[current] = swap
+  }
 }
